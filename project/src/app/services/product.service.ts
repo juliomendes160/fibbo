a61858f6940b005e779c9b2d0bf84b0baaf5a261
Julio Mendes <juliomendes160@hotmail.com>
2024-07-23 16:55:56 -0300

    Serviço de Produto

--- project/src/app/services/product.service.ts
+++ project/src/app/services/product.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private products: Product[] = [];
 
   constructor() { }
+
+  getProducts(): Product[] {
+    return this.products;
+  }
+
+  addProduct(product: Product) {
+    this.products.push(product);
+  }
+
+  updateProduct(updatedProduct: Product) {
+    const index = this.products.findIndex(p => p.id === updatedProduct.id);
+    if (index !== -1) {
+      this.products[index] = updatedProduct;
+    }
+  }
+
+  deleteProduct(id: number) {
+    this.products = this.products.filter(product => product.id !== id);
+  }
 }
