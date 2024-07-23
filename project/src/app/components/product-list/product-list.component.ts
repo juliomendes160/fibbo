4745affaf0d840977d3021d78aefe24527fc2d45
Julio Mendes <juliomendes160@hotmail.com>
2024-07-23 16:57:08 -0300

    Componente de Lista de Produtos

--- project/src/app/components/product-list/product-list.component.ts
+++ project/src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
 @Component({
   selector: 'app-product-list',
   standalone: true,
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
+
+  deleteProduct(id: number) {
+    this.productService.deleteProduct(id);
+    this.products = this.productService.getProducts();
+  }
 }
